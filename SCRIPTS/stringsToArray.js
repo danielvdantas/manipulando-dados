aafcba3747f8f86d02aae12de681bd6dc20fc0a0
danielvdantas <danielvdantas@hotmail.com>
2022-07-06 09:33:00 -0300

    Fim das atividades de Manipulação de dados

---
+++ SCRIPTS/stringsToArray.js
@@ -0,0 +1,7 @@
+// Manipulando Arrays
+
+// Transforme uma cadeia de caracteres em um Array
+
+let word = "manipulação"
+
+  console.log(Array.from(word)) // Método from recebe uma string e o Array manipula a string.
